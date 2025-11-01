d80e79a44482439da5336cacd43e21d679560794
Casterly <casterly.works@gmail.com>
2025-11-01 12:25:50 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/battleship',
+  base: '/battleship/',
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
